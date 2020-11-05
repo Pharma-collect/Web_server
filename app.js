18640b7edb57e4fcbda1747afaa66b8b8dff4bac
Lilian Giraudo <lilian.giraudo@gmail.com>
2020-11-05 17:35:16 +0100

    test route GET et POST

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express(); //create express application
 
-//middleware = range of functions
+//principal middleware element
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//You can put "use" which allows you to call in post or in get.
+
+//Postman : send data in x-www-form-urlencoded or raw with JSON settings
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
+});
+
+//get specifically
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = {
+        result: [
+            {
+                _id: 1,
+                title: 'Mon premier objet',
+                description: 'Les infos de mon premier objet',
+                price: 4900,
+                userId: 3,
+            },
+            {
+                _id: 2,
+                title: 'Mon deuxième objet',
+                description: 'Les infos de mon deuxième objet',
+                price: 2900,
+                userId: 3,
+            },
+        ],
+        status: "success",
+    };
+    res.status(200).json(stuff);
+});
+
+module.exports = app;
+
+/*//middleware = range of functions
 //first middleware element
 app.use((req, res, next) => {
     console.log('Requête reçue !');
@@ -23,6 +70,4 @@ app.use((req, res, next) => {
 //last middleware element
 app.use((req, res) => {
     console.log('Réponse envoyée avec succès !');
-});
-
-module.exports = app;
+});*/
