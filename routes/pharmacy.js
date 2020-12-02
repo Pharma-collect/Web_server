c71b94fb317ada8e62e35250dccee07c8b6c5daf
Lilian Giraudo <lilian.giraudo@gmail.com>
2020-12-02 22:08:35 +0100

    mise en place controllers

--- routes/pharmacy.js
+++ routes/pharmacy.js
@@ -1,39 +1,7 @@
 const express = require('express')
 const router = express.Router();
-const db = require('../models');
+const pharmacyController = require('../controllers/pharmacy')
 
-router.get('/getPharmacyById', function(req, res, next) {
-    const {
-        pharmacy_id
-    } = req.body;
-
-    if (!pharmacy_id){
-        res.json({
-            success: false,
-            error: "Merci de préciser un id"
-        })
-    } else {
-        db.pharmacy.findAll({
-            where: {
-                id: pharmacy_id,
-            }
-        }).then(function(result){
-            if (result.length === 0){
-                res.json({
-                    success: true,
-                    error: "Cette pharmacie n'existe pas",
-                })
-            } else {
-                res.json({
-                    success: true,
-                    result: result,
-                })
-            }
-        }).catch(error => res.json({
-            success: false,
-            error: error
-        }));
-    }
-});
+router.get('/getPharmacyById', pharmacyController.getPharmacyById);
 
 module.exports = router;
