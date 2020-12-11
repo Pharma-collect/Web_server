9bb6e8463643bb9bd6c5e2baba9184b327f34e4c
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-12-11 14:27:52 +0100

    Pharmacy webservices
    createPharmacy

--- controllers/pharmacy.js
+++ controllers/pharmacy.js
@@ -33,3 +33,44 @@ exports.getPharmacyById = function(req, res, next) {
         }));
     }
 }
+
+exports.createPharmacy = function(req, res, next) {
+    const {
+        name, 
+        has_shop,
+        road_nb,
+        road,
+        phone, 
+        post_code, 
+        city, 
+        boss
+    } = req.body;
+
+    if (!name || !has_shop || !road_nb || !road || !phone || !post_code || !city || !boss){
+        res.json({
+            success: false,
+            error: "Données manquantes"
+        })
+    } else {
+        
+            db.pharmacy.create({
+                name: name, 
+                has_shop: has_shop,
+                road_nb : road_nb,
+                road : road,
+                phone : phone, 
+                post_code : post_code, 
+                city : city, 
+                boss : boss
+            }).then(function(result){
+                res.json({
+                    success: true,
+                    result: result,
+                })
+            }).catch(error => res.json({
+                success: false,
+                error: "Informations erronées"
+            }));
+        
+    }
+}
