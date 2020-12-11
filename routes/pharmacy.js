785894d9c5b999e1779b599eb7e8cc00aa641491
Morgane Gougeon <gougeon.morgane@gmail.com>
2020-12-11 15:11:06 +0100

    Pharmacy web services
    getPharmacyByName
    getPharmacyByCity
    getPharmacyByPostCode
    getPharmacyByBoss
    getPharmacyWithShop
    getPharmacyWithoutShop
    updatePharmacyNameById
    updatePharmacyShopById
    updatePharmacyBossById
    updatePharmacyPhoneById
    deletePharmacyById
    deletePharmacyByBoss

--- routes/pharmacy.js
+++ routes/pharmacy.js
@@ -3,6 +3,19 @@ const router = express.Router();
 const pharmacyController = require('../controllers/pharmacy')
 
 router.get('/getPharmacyById', pharmacyController.getPharmacyById);
+router.get('/getPharmacyByName', pharmacyController.getPharmacyByName);
+router.get('/getPharmacyByCity', pharmacyController.getPharmacyByCity);
+router.get('/getPharmacyByPostCode', pharmacyController.getPharmacyByPostCode);
+router.get('/getPharmacyByBoss', pharmacyController.getPharmacyByBoss);
+router.get('/getPharmacyWithShop', pharmacyController.getPharmacyWithShop);
+router.get('/getPharmacyWithoutShop', pharmacyController.getPharmacyWithoutShop);
 router.post('/createPharmacy', pharmacyController.createPharmacy);
+router.post('/updatePharmacyNameById', pharmacyController.updatePharmacyNameById);
+router.post('/updatePharmacyShopById', pharmacyController.updatePharmacyShopById);
+router.post('/updatePharmacyBossById', pharmacyController.updatePharmacyBossById);
+router.post('/updatePharmacyPhoneById', pharmacyController.updatePharmacyPhoneById);
+router.post('/deletePharmacyById', pharmacyController.deletePharmacyById);
+router.post('/deletePharmacyByBoss', pharmacyController.deletePharmacyByBoss);
+
 
 module.exports = router;
