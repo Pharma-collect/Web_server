af6ac3a458cf3f6183f42068a3b411fc3e6e8196
Lilian Giraudo <lilian.giraudo@gmail.com>
2021-01-13 14:04:08 +0100

    patch All v2.1

--- routes/container.js
+++ routes/container.js
@@ -3,23 +3,17 @@ const router = express.Router();
 const containerController = require("../controllers/container");
 
 
-router.get('/getContainerById', containerController.getContainerById);
+router.post('/getContainerById', containerController.getContainerById);
 
-router.get('/getContainerByPharmacy', containerController.getContainerByPharmacy);
+router.post('/getContainerByPharmacy', containerController.getContainerByPharmacy);
 
-router.get('/getEmptyContainerByPharmacy', containerController.getEmptyContainerByPharmacy);
+router.post('/getEmptyContainerByPharmacy', containerController.getEmptyContainerByPharmacy);
 
-router.get('/getContainerStatusById', containerController.getContainerStatusById);
-
-router.get('/getContainerNumberById', containerController.getContainerNumberById);
-
-router.get('/getContainerPharmacyById', containerController.getContainerPharmacyById);
-
-router.get('/getAllContainers', containerController.getAllContainers);
+router.post('/getAllContainers', containerController.getAllContainers);
 
 router.post('/addXContainerToPharmacy', containerController.addXContainerToPharmacy);
 
-router.post('/updateContainerStatusById', containerController.updateContainerStatusById);
+router.post('/updateContainer', containerController.updateContainer);
 
 router.post('/deleteContainerById', containerController.deleteContainerById);
 
